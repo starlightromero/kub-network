559675f944ed6be8320cf7fdf762605405ad6439
Starlight Romero <starlightromero@protonmail.com>
2021-05-19 16:59:43 -0700

    [update] TaskList with error handling

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -5,12 +5,14 @@ import './TaskList.css';
 function TaskList(props) {
   return (
     <ul>
-      {props.tasks.map((task) => (
-        <li key={task.title}>
-          <h2>{task.title}</h2>
-          <p>{task.text}</p>
-        </li>
-      ))}
+      {props.tasks && props.tasks.length > 0
+        ? props.tasks.map((task) => (
+          <li key={task.title}>
+            <h2>{task.title}</h2>
+            <p>{task.text}</p>
+          </li>
+        )
+      ) : "No tasks."}
     </ul>
   );
 }
