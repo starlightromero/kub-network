7f5ea3aa950565c27da2d3fa2c7d6c46fae01df1
Starlight Romero <starlightromero@protonmail.com>
2021-05-19 15:58:24 -0700

    [update] tasks-api headers

--- tasks-api/tasks-app.js
+++ tasks-api/tasks-app.js
@@ -11,6 +11,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origins', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+})
+
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
     throw new Error('No token provided.');
